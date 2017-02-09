cd36a0909c082e988c8799bef2841d0b28f28f22
4ndr3wch4n <chan.andrew1995@gmail.com>
2017-02-08 22:01:00 -0400

    Added a bit more info

--- scripts/weathermap.js
+++ scripts/weathermap.js
@@ -1,3 +1,5 @@
+var kelvin;
+
 window.onload = function() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
@@ -5,7 +7,7 @@ window.onload = function() {
 			display(this);
 		}
 	};
-	xhttp.open("GET", "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=e06d681eb49284222ec6c140b7cdf370&mode=xml", true);
+	xhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=toronto&APPID=e06d681eb49284222ec6c140b7cdf370&mode=xml", true);
 	xhttp.send();
 }
 
@@ -13,10 +15,27 @@ function display(xml) {
 	var i, j;
 	var txt = "";
 	var xmlDoc = xml.responseXML;
-	var city = xmlDoc.getElementsByTagName("name")[0].childNodes[0];
+	var city = xmlDoc.getElementsByTagName("city")[0].getAttribute("name");
 	var country = xmlDoc.getElementsByTagName("country")[0].childNodes[0];
 	var temperature = xmlDoc.getElementsByTagName("temperature")[0].getAttribute("value");
-	document.getElementById("city").innerHTML = city.nodeValue;
+	var icon = xmlDoc.getElementsByTagName("weather")[0].getAttribute("icon");
+	var weatherValue = xmlDoc.getElementsByTagName("weather")[0].getAttribute("value");
+	document.getElementById("city").innerHTML = city;
 	document.getElementById("country").innerHTML = country.nodeValue;
-	document.getElementById("temperature").innerHTML = temperature;
+	document.getElementById("icon").innerHTML = "<img src=\"http://openweathermap.org/img/w/" + icon + ".png\" alt=\"weather icon\">";
+	document.getElementById("temperature").innerHTML = Math.round(temperature - 273.15);
+	//document.getElementById("temperature").innerHTML = Math.round(temperature * (9/5) - 459.67);
+	document.getElementById("weatherValue").innerHTML = weatherValue;
+	kelvin = temperature;
+}
+
+document.getElementById("celsius").addEventListener("click", changeCelsius);
+document.getElementById("fahrenheit").addEventListener("click", changeFahrenheit);
+
+function changeCelsius() {
+	document.getElementById("temperature").innerHTML = Math.round(kelvin - 273.15);
+}
+
+function changeFahrenheit() {
+	document.getElementById("temperature").innerHTML = Math.round(kelvin * (9/5) - 459.67);
 }
